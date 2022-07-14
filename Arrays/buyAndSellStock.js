2096d7f8ef6936830ad271767c1d550c4f121db2
TuckerTech901 <TuckerTech901@gmail.com>
2022-07-13 23:15:54 -0400

    more optimal answer included

--- Arrays/buyAndSellStock.js
+++ Arrays/buyAndSellStock.js
@@ -39,3 +39,24 @@
    
     
 }
+// Alternative (faster and more optimal)
+// var maxProfit = function(prices) {
+
+// let profit = 0;
+
+// 	let stockToBuy = prices[0];
+
+// 	for (let i = 1; i < prices.length; i++) {
+// 		if (stockToBuy > prices[i]) {
+// 			stockToBuy = prices[i];
+// 		}
+
+// 		const currentProfit = prices[i] - stockToBuy;
+
+// 		if (currentProfit > profit) {
+// 			profit = currentProfit;
+// 		}
+// 	}
+
+// 	return profit;
+// };
